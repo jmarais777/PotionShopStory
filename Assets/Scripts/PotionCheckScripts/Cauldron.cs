242de7c176819bd9c46c5f3378b6ea3381fff73f
VescentKith <brat_style@hotmail.com>
2026-08-24 22:18:11 +0200

    Cauldron Bug Fix
    - Adjusted cauldron collider size

--- Assets/Scripts/PotionCheckScripts/Cauldron.cs
+++ Assets/Scripts/PotionCheckScripts/Cauldron.cs
@@ -71,9 +71,12 @@ public class Cauldron : MonoBehaviour
         if (!_isPlayerInRange) return;
         if (currentRecipe == null) return; // makin sure there is a recipe attached
 
+        Debug.Log("The test is being performed!");
+
         ingredientsInCauldron.RemoveAll(g => g == null || !g.activeInHierarchy); // getting rid of disabled game objects
 
         List<string> namesInCauldron = ingredientsInCauldron.Select(g => g.name.Replace(" (1)", "")).ToList(); // Makes sure even copied objects register by removing the (1) at the end of its name
+
 
         bool success = currentRecipe.requiredIngredientNames.All(required => namesInCauldron.Contains(required));
 //                       && namesInCauldron.Count == currentRecipe.requiredIngredientNames.Count;
