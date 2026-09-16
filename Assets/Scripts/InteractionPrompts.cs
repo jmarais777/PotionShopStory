7503d62254ad158fefc50f577da044fc5909f55e
robotarms <2804461@students.wits.ac.za>
2026-09-16 13:30:37 +0200

    added labels and recipe UI

--- Assets/Scripts/InteractionPrompts.cs
+++ Assets/Scripts/InteractionPrompts.cs
@@ -8,21 +8,21 @@ public class InteractionPrompts : MonoBehaviour
 {
     public GameObject buttonObject; // get the buttons
     
-    //public GameObject recipePages; //refers to the UI to summon
+    public GameObject recipePages; //refers to the UI to summon
 
-    //private bool playerInBox = false; //condition related to box collider
+    private bool playerInBox = false; //condition related to box collider
 
     void Start()
     {
         buttonObject.SetActive(false); //prompts start off invisible
-        //recipePages.SetActive(false); //the recipe UI is invisible
+        recipePages.SetActive(false); //the recipe UI is invisible
     }
 
     private void OnTriggerEnter(Collider other) //when the player enters the box collider
     {
         if(other.CompareTag("Player")) //specifically the player
         {
-            //playerInBox = true; //condition is met
+            playerInBox = true; //condition is met
             buttonObject.SetActive(true); //prompts appear
         }
         else
@@ -35,19 +35,19 @@ public class InteractionPrompts : MonoBehaviour
     {
         if(other.CompareTag("Player")) //specifically the playeer
         {
-          // playerInBox = false; //condition is not met
+           playerInBox = false; //condition is not met
             buttonObject.SetActive(false); //prompts disappear
-           // recipePages.SetActive(false); //recipe UI disappears/will not work
+            recipePages.SetActive(false); //recipe UI disappears/will not work
         }
 }
 
-//public void OnEButton(InputAction.CallbackContext context) //when the E button is pressed (keyboard)
-//{
-    //if(context.performed && playerInBox) //if the E button is pressed, and the player is in the box collider, and if the "physical letter" exists
-    //{
-      //  recipePages.SetActive(true);
-   // }
+public void OnEButton(InputAction.CallbackContext context) //when the E button is pressed (keyboard)
+{
+    if(context.performed && playerInBox) //if the E button is pressed, and the player is in the box collider, and if the "physical letter" exists
+    {
+        recipePages.SetActive(!recipePages.activeSelf);
+    }
 
-//}
+}
 
 }
